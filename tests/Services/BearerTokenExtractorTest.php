ca940460f594e07a9a858d339a3a62c81c069657
Tom Roar Furunes <tom.roar@halloverden.no>
2022-07-06 17:45:05 +0200

    ADD: BearerTokenExtractorTest

---
+++ tests/Services/BearerTokenExtractorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HalloVerden\JwtAuthenticatorBundle\Tests\Services;
+
+use HalloVerden\JwtAuthenticatorBundle\JwtExtractor\BearerTokenExtractor;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class BearerTokenExtractorTest extends TestCase {
+
+  public function testExtractToken_requestWithToken_shouldReturnToken() {
+    $request = new Request();
+    $request->headers->set('Authorization', 'Bearer test_token');
+
+    $bearerTokenExtractor = new BearerTokenExtractor();
+    $token = $bearerTokenExtractor->extractToken($request);
+
+    $this->assertEquals('test_token', $token);
+  }
+
+  public function testExtractToken_requestWithoutToken_shouldReturnNull() {
+    $request = new Request();
+
+    $bearerTokenExtractor = new BearerTokenExtractor();
+    $token = $bearerTokenExtractor->extractToken($request);
+
+    $this->assertNull($token);
+  }
+
+}
