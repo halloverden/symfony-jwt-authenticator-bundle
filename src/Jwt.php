f83c4cb0ff02b79d2137b52f566908d339701241
Tom Roar Furunes <tom.roar@halloverden.no>
2022-07-06 15:23:58 +0200

    ADD: Jwt

---
+++ src/Jwt.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace HalloVerden\JwtAuthenticatorBundle;
+
+final class Jwt {
+
+  /**
+   * Jwt constructor.
+   */
+  public function __construct(
+    private readonly array $claims,
+    private readonly array $headers
+  ) {
+  }
+
+  /**
+   * @return array
+   */
+  public function getClaims(): array {
+    return $this->claims;
+  }
+
+  /**
+   * @param string $key
+   *
+   * @return mixed
+   */
+  public function getClaim(string $key): mixed {
+    return $this->claims[$key] ?? null;
+  }
+
+  /**
+   * @return array
+   */
+  public function getHeaders(): array {
+    return $this->headers;
+  }
+
+  /**
+   * @param string $key
+   *
+   * @return mixed
+   */
+  public function getHeader(string $key): mixed {
+    return $this->headers[$key] ?? null;
+  }
+
+}
